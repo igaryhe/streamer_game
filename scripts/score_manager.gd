f357b216c5358cf2b140da633b1675e23549612e
dan <i@dan.games>
2025-04-23 17:32:51 +0800

    add data for scores

---
+++ scripts/score_manager.gd
@@ -0,0 +1,33 @@
+extends Node
+
+@export var init_score: float
+@export var warmup_duration: float
+@export var natural_decay_speed: float
+
+# for the tiny figure UI
+@export var positive_state_threshold: float
+@export var neutural_state_threshold: float
+@export var negative_state_threshold: float
+
+# selfie
+@export var selfie_threshold: float
+@export var selfie_increase_speed: float
+@export var selfie_check_interval: float
+@export var selfie_caught_rate_increase_speed: float
+@export var selfie_caught_rate_decrease_speed: float
+
+var score: float
+
+var selfie_caught_rate: float
+
+var ghost_score: float
+var ghost_score_threshold: float
+
+var chat_refresh_speed: float
+
+# 0 is warmup bucket
+# 1 is boring bucket
+# 2 is paranormal event bucket
+# 3 is selfie bucket
+# 4 is sacrifice bucket
+@export var chat_bucket_weights: Array[float]
