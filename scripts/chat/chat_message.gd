a564a0d6341a73c575d26097910dd563337ac299
vipsbpig <vipsbpig@gmail.com>
2025-04-23 21:22:59 +0800

    small update on chat message

--- scripts/chat/chat_message.gd
+++ scripts/chat/chat_message.gd
@@ -2,11 +2,26 @@ extends Control
 
 enum MessageType { MESSAGE, DONATION, DONATION_TEXT }
 
+@export var bg_0 : Control
+@export var bg_1 : Control
+@export var bg_2 : Control
+	
 @export var content_label : RichTextLabel
 var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 
-func set_content(text: String, type: int) -> void:
+func set_content(text: String, type: MessageType) -> void:
+	bg_0.visible = false; bg_1.visible = false; bg_2.visible = false
+	match type:
+		MessageType.MESSAGE:
+			bg_0.visible = true
+			content_label.visible = true
+		MessageType.DONATION:
+			bg_1.visible = true	
+			content_label.visible = false
+		MessageType.DONATION_TEXT:
+			bg_2.visible = true
+			content_label.visible = true
 	content_label.text = random_name() + ": " + text
 	
 func random_name() -> String:
