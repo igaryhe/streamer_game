b15417a437fe83b3ceb2066ccb5909c6261df7f9
dan <i@dan.games>
2025-05-02 16:52:40 +0800

    update forest face

---
+++ scripts/forest_face.gd
@@ -0,0 +1,17 @@
+extends Node3D
+
+@export var range: Vector2
+@export var sprite: Sprite3D
+@export var player: Node3D
+@export var collider: StaticBody3D
+
+func _ready() -> void:
+	collider.process_mode = Node.PROCESS_MODE_DISABLED
+
+func _process(delta: float):
+	var distance = (player.global_position - sprite.global_position).length()
+	sprite.modulate.a = (clampf(distance, range.x, range.y) - range.x) / (range.y - range.x)
+	if sprite.modulate.a == 0:
+		collider.process_mode = Node.PROCESS_MODE_INHERIT
+	else:
+		collider.process_mode = Node.PROCESS_MODE_DISABLED
--- scripts/shadow.gd
+++ scripts/shadow.gd
@@ -1,6 +1,7 @@
 extends Node3D
 
 @export var sprite: Sprite3D
+@export var collider: StaticBody3D
 @export var interval: float
 @export var visible_duration: float
 @export var transform_duration: float
@@ -30,11 +31,14 @@ func _process(delta: float):
 			timer = interval
 	elif state == 0:
 		sprite.modulate.a = 0
+		collider.process_mode = Node.PROCESS_MODE_DISABLED
 		if timer <= 0:
 			state = 2
 			timer = transform_duration
 	elif state == 1:
 		sprite.modulate.a = 1
+		collider.process_mode = Node.PROCESS_MODE_INHERIT
+		collider.visible = true
 		if timer <= 0:
 			state = 3
 			timer = transform_duration
