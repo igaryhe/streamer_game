9aa486c50d9554717eb4664c4961fa1961b145a6
vipsbpig <vipsbpig@gmail.com>
2025-05-03 23:17:14 +0800

    chat message

--- scripts/chat/chat_message.gd
+++ scripts/chat/chat_message.gd
@@ -1,15 +1,15 @@
 extends Control
 class_name ChatMessage
 
-enum MessageType { MESSAGE, DONATION, DONATION_TEXT }
+enum MessageType {MESSAGE, DONATION, DONATION_TEXT, DONATION_TEXT_MAX}
 
-@export var bg_0 : Control
-@export var bg_1 : Control
-@export var bg_2 : Control
+@export var bg_0: Control
+@export var bg_1: Control
+@export var bg_2: Control
 	
-@export var content_label : RichTextLabel
+@export var content_label: RichTextLabel
 
-@export var total_chars :int = 25
+@export var total_chars: int = 25
 var chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789"
 
 
@@ -20,7 +20,7 @@ func set_content(text: String, type: MessageType) -> void:
 			bg_0.visible = true
 			content_label.visible = true
 		MessageType.DONATION:
-			bg_1.visible = true	
+			bg_1.visible = true
 			content_label.visible = false
 		MessageType.DONATION_TEXT:
 			bg_2.visible = true
@@ -28,8 +28,8 @@ func set_content(text: String, type: MessageType) -> void:
 	
 	content_label.text = random_name(total_chars - len(text) - 2) + ": " + text
 	
-func random_name(force_total:int = -1) -> String:
-	var star_count = randi_range(2,5)
+func random_name(force_total: int = -1) -> String:
+	var star_count = randi_range(2, 5)
 	var first = chars[randi() % len(chars)]
 	var last = chars[randi() % len(chars)]
 	var max_count = 8 if force_total >= 2 else force_total
