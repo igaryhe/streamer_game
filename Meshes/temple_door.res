9e27f20bf07ebfb7d15f8bfee82ddc9679082ff1
bakin1-admin <ba3kinnn@gmail.com>
2025-05-03 23:14:19 +0800

    chat update

--- Meshes/temple_door.res
+++ Meshes/temple_door.res
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:910d008563aded53221abdb2c308cd5711c964676c960f93b35de21846f6276a
-size 12117
+oid sha256:0401d560735bbd960a018898ff3190000b8187f27ee807cb4ee5dd8decc8deb2
+size 5551
--- data/chats_stream_begin.gd
+++ data/chats_stream_begin.gd
@@ -9,6 +9,6 @@ var contents = [
 	[1,"今天播什么啊", ChatMessage.MessageType.MESSAGE],
 	[1,"主播怎么找到的这地方", ChatMessage.MessageType.MESSAGE],
 	[1,"这是之前很火的那个村子吗", ChatMessage.MessageType.MESSAGE],
-	[1,"这是哪里", ChatMessage.MessageType.MESSAGE],
+	[1,"什么地方", ChatMessage.MessageType.MESSAGE],
 	[1,"好耶", ChatMessage.MessageType.DONATION_TEXT],
 ]
